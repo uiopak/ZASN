c04a31c54500e6fd0edbd19adbaac65f775bf3f1
uiopak <44174807+uiopak@users.noreply.github.com>
2022-05-05 08:22:10 +0200

    graph-wavenet wandb.ai

--- Graph-WaveNet/train.py
+++ Graph-WaveNet/train.py
@@ -5,6 +5,17 @@ import time
 import util
 import matplotlib.pyplot as plt
 from engine import trainer
+import wandb
+
+wandb.init(project="test-project-graph-wavenet", entity="pg-test-zasn", config={
+  "learning_rate": 0.001,
+  "epochs": 16,
+  "batch_size": 64,
+  "dropout": 0.3,
+  "weight_decay": 0.0001
+})
+
+config = wandb.config
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--device',type=str,default='cuda:0',help='')
@@ -41,7 +52,7 @@ def main():
     #load data
     device = torch.device(args.device)
     sensor_ids, sensor_id_to_ind, adj_mx = util.load_adj(args.adjdata,args.adjtype)
-    dataloader = util.load_dataset(args.data, args.batch_size, args.batch_size, args.batch_size)
+    dataloader = util.load_dataset(args.data, config.batch_size, config.batch_size, config.batch_size)
     scaler = dataloader['scaler']
     supports = [torch.tensor(i).to(device) for i in adj_mx]
 
@@ -57,8 +68,8 @@ def main():
 
 
 
-    engine = trainer(scaler, args.in_dim, args.seq_length, args.num_nodes, args.nhid, args.dropout,
-                         args.learning_rate, args.weight_decay, device, supports, args.gcn_bool, args.addaptadj,
+    engine = trainer(scaler, args.in_dim, args.seq_length, args.num_nodes, args.nhid, config.dropout,
+                         config.learning_rate, config.weight_decay, device, supports, args.gcn_bool, args.addaptadj,
                          adjinit)
 
 
@@ -66,7 +77,7 @@ def main():
     his_loss =[]
     val_time = []
     train_time = []
-    for i in range(1,args.epochs+1):
+    for i in range(1,config.epochs+1):
         #if i % 10 == 0:
             #lr = max(0.000002,args.learning_rate * (0.1 ** (i // 10)))
             #for g in engine.optimizer.param_groups:
@@ -76,7 +87,10 @@ def main():
         train_rmse = []
         t1 = time.time()
         dataloader['train_loader'].shuffle()
-        for iter, (x, y) in enumerate(dataloader['train_loader'].get_iterator()):
+        iter = dataloader['train_loader'].get_iterator()
+        max_steps = sum(1 for _ in iter)
+        iter = dataloader['train_loader'].get_iterator()
+        for iter, (x, y) in enumerate(iter):
             trainx = torch.Tensor(x).to(device)
             trainx= trainx.transpose(1, 3)
             trainy = torch.Tensor(y).to(device)
@@ -85,8 +99,15 @@ def main():
             train_loss.append(metrics[0])
             train_mape.append(metrics[1])
             train_rmse.append(metrics[2])
-            if iter % args.print_every == 0 :
+            metrics2 = {
+                    "train/train_loss": train_loss[-1], 
+                    "train/train_mape": train_mape[-1], 
+                    "train/train_rmse": train_rmse[-1], 
+                    "train/epoch": (iter + (max_steps * (i-1))) / max_steps
+                    }
+            if iter % args.print_every == 0:
                 log = 'Iter: {:03d}, Train Loss: {:.4f}, Train MAPE: {:.4f}, Train RMSE: {:.4f}'
+                wandb.log(metrics2)
                 print(log.format(iter, train_loss[-1], train_mape[-1], train_rmse[-1]),flush=True)
         t2 = time.time()
         train_time.append(t2-t1)
@@ -119,15 +140,23 @@ def main():
         mvalid_rmse = np.mean(valid_rmse)
         his_loss.append(mvalid_loss)
 
+        # 🐝 Log train and validation metrics to wandb
+        val_metrics = {
+            "val/val_loss": mvalid_loss, 
+            "val/val_mape": mvalid_mape,
+            "val/val_rmse": mvalid_rmse,
+        }
+        wandb.log({**metrics2, **val_metrics})
+
         log = 'Epoch: {:03d}, Train Loss: {:.4f}, Train MAPE: {:.4f}, Train RMSE: {:.4f}, Valid Loss: {:.4f}, Valid MAPE: {:.4f}, Valid RMSE: {:.4f}, Training Time: {:.4f}/epoch'
         print(log.format(i, mtrain_loss, mtrain_mape, mtrain_rmse, mvalid_loss, mvalid_mape, mvalid_rmse, (t2 - t1)),flush=True)
-        torch.save(engine.model.state_dict(), args.save+"_epoch_"+str(i)+"_"+str(round(mvalid_loss,2))+".pth")
+        torch.save(engine.model.state_dict(), args.save+"_epoch_"+str(i)+"_"+str(round(mvalid_loss,2))+"_"+str(config.learning_rate)+"_"+str(config.batch_size)+"_"+str(config.dropout)+"_"+str(config.dropout)+".pth")
     print("Average Training Time: {:.4f} secs/epoch".format(np.mean(train_time)))
     print("Average Inference Time: {:.4f} secs".format(np.mean(val_time)))
 
     #testing
     bestid = np.argmin(his_loss)
-    engine.model.load_state_dict(torch.load(args.save+"_epoch_"+str(bestid+1)+"_"+str(round(his_loss[bestid],2))+".pth"))
+    engine.model.load_state_dict(torch.load(args.save+"_epoch_"+str(bestid+1)+"_"+str(round(his_loss[bestid],2))+"_"+str(config.learning_rate)+"_"+str(config.batch_size)+"_"+str(config.dropout)+"_"+str(config.dropout)+".pth"))
 
 
     outputs = []
@@ -163,8 +192,13 @@ def main():
         armse.append(metrics[2])
 
     log = 'On average over 12 horizons, Test MAE: {:.4f}, Test MAPE: {:.4f}, Test RMSE: {:.4f}'
+    wandb.summary['test_MAE'] = np.mean(amae)
+    wandb.summary['test_MAPE'] = np.mean(amape)
+    wandb.summary['test_RMSE'] = np.mean(armse)
     print(log.format(np.mean(amae),np.mean(amape),np.mean(armse)))
-    torch.save(engine.model.state_dict(), args.save+"_exp"+str(args.expid)+"_best_"+str(round(his_loss[bestid],2))+".pth")
+    torch.save(engine.model.state_dict(), args.save+"_exp"+str(args.expid)+"_best_"+str(round(his_loss[bestid],2))+"_"+str(config.learning_rate)+"_"+str(config.batch_size)+"_"+str(config.dropout)+"_"+str(config.dropout)+".pth")
+
+    wandb.finish()
 
 
 
