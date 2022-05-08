e92bf09c0989d2ef0f58401bd2943f29e20f685e
uiopak <44174807+uiopak@users.noreply.github.com>
2022-05-08 11:36:58 +0200

    graph use support test 2

--- Graph-WaveNet/train.py
+++ Graph-WaveNet/train.py
@@ -37,7 +37,7 @@ parser.add_argument('--weight_decay',type=float,default=0.0001,help='weight deca
 parser.add_argument('--epochs',type=int,default=100,help='')
 parser.add_argument('--print_every',type=int,default=50,help='')
 #parser.add_argument('--seed',type=int,default=99,help='random seed')
-parser.add_argument('--save',type=str,default='./garage/metrtestusesupport',help='save path')
+parser.add_argument('--save',type=str,default='./garage/metrtestusesupport2',help='save path')
 parser.add_argument('--expid',type=int,default=1,help='experiment id')
 
 args = parser.parse_args()
